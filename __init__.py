bdbf675d265fd13d5eb5931a40b08ce4e8e178d4
Bogdan Brezuica <brezuicabogdan@gmail.com>
2020-05-02 18:54:30 +0100

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,10 +1,16 @@
 from mycroft import MycroftSkill, intent_file_handler
 
+__author__ = 'BreziCode'
+
 
 class QuietHours(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def initialize(self):
+        if(self.settings.get())
+ 
+    
     @intent_file_handler('hours.quiet.intent')
     def handle_hours_quiet(self, message):
         self.speak_dialog('hours.quiet')
@@ -12,4 +18,3 @@ class QuietHours(MycroftSkill):
 
 def create_skill():
     return QuietHours()
-
