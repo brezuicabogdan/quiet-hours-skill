b87ab861a4dc8bd3a7f4bb85852e884135ab46c1
Bogdan Brezuica <brezuicabogdan@gmail.com>
2020-05-12 19:52:28 +0100

    Fixed issue with crash when Mixer was not properly initialized Shutdown process now frees up Mixer object Fixed issue with mupliple executions if initialization method when settings changed or skill restarted

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.util.time import to_local, now_local, to_system
@@ -8,6 +8,7 @@ from mycroft.audio import wait_while_speaking
 
 __author__ = 'BreziCode'
 
+
 class QuietHours(MycroftSkill):
 
     ON_EVENT_NAME = 'quiet_hours_on'
@@ -15,7 +16,12 @@ class QuietHours(MycroftSkill):
 
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.log.debug("Executing __init__")
         self.init_settings()
+        self._get_mixer()
+
+    def _get_mixer(self):
+        self.log.debug("Executing _get_mixer")
         try:
             self.mixer = Mixer()
         except Exception:
@@ -25,22 +31,28 @@ class QuietHours(MycroftSkill):
             except Exception as e:
                 self.log.error('Couldn\'t allocate mixer, {}'.format(repr(e)))
                 self.mixer = None
-        self.saved_volume = self.mixer.getvolume() 
+        if (self.mixer is not None):
+            self.saved_volume = self.mixer.getvolume()
 
-    def initialize(self): 
+    def initialize(self):
+        self.log.debug("Executing initialize")
         self.start_time = None
         self.end_time = None
+        self.detailed_message_cnt = 5
+        self.saved_volume = None 
 
         self.settings_change_callback = self._init
         self.add_event('private.mycroftai.quiet_hours', self.on_quiet_hours)
 
-        # self._init()
+        self._init() 
 
     def _init(self):
+        self.log.debug("Executing _init")
         self.clear_events()
         if(self.settings.get('enabled')):
             self.log.info("Quiet hours skill is ENABLED")
-            self.log.debug("Quiet hours mode is: {}".format(self.settings.get('active')))
+            self.log.debug("Quiet hours mode is: {}".format(
+                self.settings.get('active')))
             self.log.debug("Saved volume is: {}".format(self.saved_volume))
             self.set_start_end()
             self.set_events()
@@ -56,14 +68,17 @@ class QuietHours(MycroftSkill):
             if(self.settings.get('active')):
                 self.log.debug('Init: turning off because not enabled')
                 self.off()
-        
+
     def shutdown(self):
+        self.log.debug("Executing shutdown")
         self.clear_events()
-        if(self.settings.get('active')):
-            self.off()           
+        self.mixer = None
 
     def init_settings(self):
         """Add any missing default settings."""
+
+        self.log.debug("Executing init_settings")
+
         self.settings.setdefault('enabled', False)
         self.settings.setdefault('start_time_hour', 22)
         self.settings.setdefault('start_time_min', 0)
@@ -72,26 +87,36 @@ class QuietHours(MycroftSkill):
         self.settings.setdefault('use_naptime', True)
         self.settings.setdefault('set_volume_to', 0)
 
+        if(self.settings.get('set_volume_to') < 0):
+            self.settings['set_volume_to'] = 0
+        elif (self.settings.get('set_volume_to') > 100):
+            self.settings['set_volume_to'] = 100
         self.settings.setdefault('active', False)
 
     def on_quiet_hours(self, message):
         self.bus.emit(message.response(
             data={"quiet_hours_on": self.settings.get('active')}))
 
-    def set_start_end(self):
+    def set_start_end(self): 
         """ Construct dattime objects from settings for start time and end time """
+        self.log.debug("Executing set_start_end") 
         now = to_local(datetime.now())
         self.start_time = now.replace(hour=int(self.settings.get('start_time_hour')),
                                       minute=int(self.settings.get('start_time_min')))
+        if(self.start_time <= now ):
+            self.start_time += timedelta(days=1)
         self.log.debug("Start time is: {}".format(
             self.start_time.strftime("%H:%M")))
         self.end_time = now.replace(hour=int(self.settings.get('end_time_hour')),
                                     minute=int(self.settings.get('end_time_min')))
+        if(self.end_time <= now ):
+            self.end_time += timedelta(days=1)
         self.log.debug("End time is: {}".format(
             self.end_time.strftime("%H:%M")))
 
     def on(self, speak=True):
-        if(not self.settings.get('enabled') or self.mixer is None ):
+        self.log.debug("Executing ON")
+        if(not self.settings.get('enabled') or self.mixer is None):
             return
         self.saved_volume = self.mixer.getvolume()
         self.log.debug("Quiet hours are in efect")
@@ -99,21 +124,25 @@ class QuietHours(MycroftSkill):
         if(speak):
             self.speak_dialog('on')
             wait_while_speaking()
-            self.speak_dialog('wake.word')
-            wait_while_speaking()
-        self.mixer.setvolume(self.settings.get('set_volume_to'))
+            if (self.detailed_message_cnt > 0):
+                self.detailed_message_cnt = self.detailed_message_cnt - 1
+                self.speak_dialog('wake.word')
+                wait_while_speaking()
+        self._set_volume(self.settings.get('set_volume_to'))
 
     def off(self, speak=True):
+        self.log.debug("Executing OFF {}".format(exception))
         if(not self.settings.get('enabled') or self.mixer is None):
             return
-        self.mixer.setvolume(self.saved_volume[0])
+        self._set_volume(self.saved_volume[0])
         self.log.debug("Quiet hours not in effect anymore")
         self.settings['active'] = False
         if(speak):
-            self.speak_dialog('off')
+            self.speak_dialog('off') 
 
     def should_turn_on_now(self):
         """ Check if current time is inside teh quiet hours interval"""
+        self.log.debug("Executing should_turn_on_now")
         now = now_local()
         self.log.debug("Curent time is {}".format(now.strftime("%H:%M")))
         if(self.start_time <= self.end_time):
@@ -122,15 +151,26 @@ class QuietHours(MycroftSkill):
             return (now > self.start_time or now < self.end_time)
 
     def clear_events(self):
+        self.log.debug("Executing clear_events")
         self.cancel_scheduled_event(self.ON_EVENT_NAME)
         self.cancel_scheduled_event(self.OFF_EVENT_NAME)
 
     def set_events(self):
-        self.log.debug('Setting events')
+        self.log.debug('Executing set_events')
         self.schedule_repeating_event(self.on, to_system(
             self.start_time), 86400, name=self.ON_EVENT_NAME)
         self.schedule_repeating_event(self.off, to_system(
             self.end_time), 86400, name=self.OFF_EVENT_NAME)
+
+    def _set_volume(self, volume): 
+        self.log.debug("Executing _setvolume")
+        if (self.mixer is None):
+            self._get_mixer
+        if (self.mixer is not None):
+            self.mixer.setvolume(volume)
+            self.log.debug("Volume set to {}".format(volume))
+        else:
+            self.log.warning("Could not set volume! Could not get mixer!")
 
     @intent_file_handler('enable.intent')
     def handle_enable_quiet_hours(self, message):
